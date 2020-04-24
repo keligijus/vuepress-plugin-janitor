c5f57335a670db33f13846f22d42b956eecf3606
Eligijus Krepsta <k.eligijus@gmail.com>
2020-04-24 19:17:35 +0300

    allow deleting folders

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = (options, ctx) => {
-    return {
-        name: 'vuepress-plugin-janitor',
+  return {
+    name: 'vuepress-plugin-janitor',
 
-        generated () {
-            ctx.pages.map(page => {
-                const currentDateUTC = new Date().toUTCString()
-                const currentDateISO = new Date(currentDateUTC).toISOString()
+    generated() {
+      ctx.pages.map((page) => {
+        const currentDateUTC = new Date().toUTCString();
+        const currentDateISO = new Date(currentDateUTC).toISOString();
 
-                if (page.frontmatter.date) {
-                    const currentDate = new Date(currentDateISO)
-                    const pageDate = new Date(page.frontmatter.date)
+        if (page.frontmatter.date) {
+          const currentDate = new Date(currentDateISO);
+          const pageDate = new Date(page.frontmatter.date);
 
-                    const isFuturePost = pageDate > currentDate
+          const isFuturePost = pageDate > currentDate;
 
-                    if (isFuturePost) {
-                        fs.unlinkSync(ctx.outDir + page.path)
+          if (isFuturePost) {
+            deleteFolderRecursive(ctx.outDir + page.path);
+            // fs.unlinkSync();
 
-                        console.log(`♻️ Future Post: ${page.frontmatter.title} was removed from the build directory.`)
-                    }
-                }
-            })
+            console.log(
+              `♻️ Future Post: ${page.frontmatter.title} was removed from the build directory.`
+            );
+          }
         }
-    }
-}
+      });
+    },
+  };
+};
+
+const deleteFolderRecursive = (path) => {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach((file) => {
+      const currentPath = `${path}/${file}`;
+
+      fs.lstatSync(currentPath).isDirectory()
+        ? deleteFolderRecursive(currentPath)
+        : fs.unlinkSync(currentPath);
+    });
+
+    fs.rmdirSync(path);
+  }
+};
